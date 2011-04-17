55dd01aa1100c8c4358de1c3451a0bcc606e1bbf
Alexander Kern <alex@kernul.com>
2011-04-16 17:06:28 -0700

    more testing

--- test/rails3/test/test_helper.rb
+++ test/rails3/test/test_helper.rb
@@ -3,9 +3,21 @@ ENV['RAILS_ENV'] ||= 'test'
 puts 'GOD!L:SKJDF:LSKJDF'
 
 require File.expand_path('../../config/environment', __FILE__)
+
+puts '!!!!!'
+
 require 'minitest/autorun'
+
+puts '67890'
+
 require 'minitest/pride'
+
+puts '574524'
+
 require 'rails/test_help'
+
+puts '`L:SKJDF:```````'
+
 require 'mocha'
 
 puts 'fa8dysgf'
